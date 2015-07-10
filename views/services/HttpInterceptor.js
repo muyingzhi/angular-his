0a4e6df40009540774ac8a2bb6799b2511ebc629
muyz <yingzhi.mu@gmail.com>
2015-07-10 11:29:48 +0800

    完善httpinterceptor的方法

--- views/services/HttpInterceptor.js
+++ views/services/HttpInterceptor.js
@@ -1,14 +1,24 @@
 define(['jquery','services/services'],function($,services){
-	
-	var s = function($rootScope){
-		this.request = function(){
-			$rootScope.message = ("正在处理请求......");
-	        $("#myModal").modal("show");
-	    }
-	}
-	services.service("HttpInterceptor",s);
-	services.config(['$httpProvider',function($httpProvider){
-		//$httpProvider.interceptors.push('HttpInterceptor');
-		$httpProvider.defaults.transformRequest.push(s);
-	}])
+	services.factory("HttpInterceptor",function($q){
+		var interceptor = {
+				'request' : function(config){
+					angular.element('#httpMsg').show(500);
+					//console.log("HTTP request:" + config.url);
+		    		return config;
+				},
+				'response': function(config){
+					angular.element('#httpMsg').hide(100);
+		    		return config;
+				},
+				'responseError': function(rejection){
+					if (rejection.status === 401) {
+						alert("访问("+rejection.config.url+")错误("+rejection.status+")："+"拒绝访问");
+			    	} else if (rejection.status >= 400 && rejection.status < 500) {
+			    		alert("访问("+rejection.config.url+")错误("+rejection.status+")："+rejection.statusText);
+			    	}
+					return $q.reject(rejection);
+				}
+		}
+		return interceptor;
+	});
 });
